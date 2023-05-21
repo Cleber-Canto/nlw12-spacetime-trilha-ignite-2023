9663e4978815e60294f3c91817477d8e81d9da66
canto-saraiva <cantosaraiva@hotmail.com>
2023-05-20 20:38:09 -0400

    Finalizado o projeto

---
+++ web/next.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['avatars.githubusercontent.com'],
+    },
+  }
+  
+  module.exports = nextConfig
